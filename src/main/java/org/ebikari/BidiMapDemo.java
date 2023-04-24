04320258c65996b00411bc244b38fb5b769ebdd5
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-04-24 12:58:57 +0100

    org.ebikari.BidiMapDemo: Test unique value behavior

--- src/main/java/org/ebikari/BidiMapDemo.java
+++ src/main/java/org/ebikari/BidiMapDemo.java
@@ -1,4 +1,22 @@
 package org.ebikari;
 
+import org.apache.commons.collections4.BidiMap;
+import org.apache.commons.collections4.bidimap.TreeBidiMap;
+
 public class BidiMapDemo {
+
+    public static void main(String[] args) {
+        // initialize map
+        BidiMap<String, Integer> bidiMap = new TreeBidiMap<>() {{
+            put("one", 1);
+            put("two", 1); // replaces the key "one"
+        }};
+
+        // "two"
+        System.out.println(bidiMap.getKey(1));
+
+        // {two=1}
+        System.out.println(bidiMap);
+
+    }
 }
